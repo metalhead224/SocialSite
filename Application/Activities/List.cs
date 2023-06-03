cd8a6adecc717f9cacf7025778e9537af480ad2f
suman osti <suman.osti401@gmail.com>
2023-06-03 19:24:40 +0545

    cancellation token

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,21 +1,22 @@
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>> {}
+        public class Query : IRequest<List<Activity>> { }
 
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
-        private readonly DataContext _context;
+            private readonly DataContext _context;
             public Handler(DataContext context)
             {
-            _context = context;
-                
+                _context = context;
+
             }
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
