ea4bc1927893bd6520c90a9ac5d3e556da63453b
suman osti <suman.osti401@gmail.com>
2023-06-10 21:21:19 +0545

    added form functionality

---
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Button, Item, ItemContent, Label, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+  activities: Activity[];
+  selectActivity: (id: string) => void;
+}
+
+export default function ActivityList({ activities, selectActivity }: Props) {
+  return (
+    <Segment>
+      <Item.Group divided>
+        {activities.map((activity) => (
+          <Item key={activity.id}>
+            <ItemContent>
+              <Item.Header as="a">{activity.title}</Item.Header>
+              <Item.Description>
+                <div>{activity.description}</div>
+                <div>
+                  {activity.city}, {activity.venue}
+                </div>
+              </Item.Description>
+              <Item.Extra>
+                <Button
+                  onClick={() => selectActivity(activity.id)}
+                  floated="right"
+                  content="view"
+                  color="blue"
+                />
+                <Label basic content={activity.category} />
+              </Item.Extra>
+            </ItemContent>
+          </Item>
+        ))}
+      </Item.Group>
+    </Segment>
+  );
+}
---
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,86 @@
+import React, { ChangeEvent, useState } from "react";
+import { Button, Form, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+  activity: Activity | undefined;
+  closeForm: () => void;
+}
+
+export default function ActivityForm({
+  activity: selectedActivity,
+  closeForm,
+}: Props) {
+  const initialState = selectedActivity ?? {
+    id: "",
+    title: "",
+    description: "",
+    category: "",
+    date: "",
+    city: "",
+    venue: "",
+  };
+
+  const [activity, setActivity] = useState(initialState);
+
+  function handleSubmit() {
+    console.log(activity);
+  }
+
+  function handleInputChange(
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    const { name, value } = event.target;
+    setActivity({ ...activity, [name]: value });
+  }
+
+  return (
+    <Segment clearing>
+      <Form onSubmit={handleSubmit} autoComplete="off">
+        <Form.Input
+          placeholder="Title"
+          name="title"
+          value={activity.title}
+          onChange={handleInputChange}
+        />
+        <Form.TextArea
+          placeholder="Description"
+          name="description"
+          value={activity.description}
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="Category"
+          name="category"
+          value={activity.category}
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="Date"
+          name="date"
+          value={activity.date}
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="City"
+          name="city"
+          value={activity.city}
+          onChange={handleInputChange}
+        />
+        <Form.Input
+          placeholder="Venue"
+          name="venue"
+          value={activity.venue}
+          onChange={handleInputChange}
+        />
+        <Button type="submit" positive floated="right" content="Submit" />
+        <Button
+          onClick={closeForm}
+          type="button"
+          floated="right"
+          content="Cancel"
+        />
+      </Form>
+    </Segment>
+  );
+}
